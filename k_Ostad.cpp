17a01920b1b383dcf9e5cbd9d5c61ba4b5ccec29
Zawad Mutawassit <zawadmann@gmail.com>
2025-02-18 04:21:58 -0330

    done

--- k_Ostad.cpp
+++ k_Ostad.cpp
@@ -7,7 +7,31 @@ using namespace std;
 
 bool isKOstad(const string& st1, const string& st2, int k)
 {
-    
+    vector<int> freq1(26, 0), freq2(26, 0);
+
+    for (char c : st1)
+    {
+        freq1[c - 'a']++; //for indexing purposes subtracting character value of "a" gives the integer index
+    }
+    for (char c : st2)
+    {
+        freq2[c - 'a']++;
+    }
+    int sumPositive = 0;
+    int sumNegative = 0;
+
+    //LOOP to check the mismatches in the strings. ONLY insert delete and change
+    for (int i = 0; i < 26; i++) {
+        int diff = freq1[i] - freq2[i];
+        if (diff > 0) {
+            sumPositive += diff;
+        } else {
+            sumNegative += (-diff);
+        }
+    }
+
+    int cost = max(sumPositive, sumNegative);
+    return (cost <= k);
 }
 
 
